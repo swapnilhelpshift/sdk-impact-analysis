cd32a205859b5d7b1d0aeb3ff621c18ba0e1fd35
Swapnil <swapnil@helpshift.com>
2016-05-16 12:54:39 +0530

    Add skeleton code for impact analysis
    TODOs:
    1. Add Helpshift Init Call in MainApplication class
    2. Add API calls in MainActivity class

--- app/src/main/java/com/helpshift/impactanalysis/MainActivity.java
+++ app/src/main/java/com/helpshift/impactanalysis/MainActivity.java
@@ -2,12 +2,44 @@ package com.helpshift.impactanalysis;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    findViews();
+  }
+
+  @SuppressWarnings("ConstantConditions")
+  private void findViews() {
+    (findViewById(R.id.showFAQs)).setOnClickListener(this);
+    (findViewById(R.id.showFAQSection)).setOnClickListener(this);
+    (findViewById(R.id.showSingleFAQ)).setOnClickListener(this);
+    (findViewById(R.id.showConversation)).setOnClickListener(this);
+    (findViewById(R.id.showInbox)).setOnClickListener(this);
+    (findViewById(R.id.showMessage)).setOnClickListener(this);
+  }
+
+  @Override
+  public void onClick(View v) {
+    //TODO: Call appropriate APIs
+    switch (v.getId()) {
+      case R.id.showFAQs:
+        break;
+      case R.id.showFAQSection:
+        break;
+      case R.id.showSingleFAQ:
+        break;
+      case R.id.showConversation:
+        break;
+      case R.id.showInbox:
+        break;
+      case R.id.showMessage:
+        break;
+    }
   }
 }
---
+++ app/src/main/java/com/helpshift/impactanalysis/MainApplication.java
@@ -0,0 +1,12 @@
+package com.helpshift.impactanalysis;
+
+import android.app.Application;
+
+public class MainApplication extends Application {
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    //TODO: Add Helpshift init code here
+  }
+}
